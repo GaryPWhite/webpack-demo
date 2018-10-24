1845c34746a99d0fa1b432d71f758edeb7482e74
Gary White Jr <gwhite@wayfair.com>
2018-10-24 14:57:53 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
+
+function component() {
+  let element = document.createElement('div');
+
+  // Lodash imported through webpack
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  let myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
